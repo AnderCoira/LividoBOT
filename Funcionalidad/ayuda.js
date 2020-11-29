be807466c2e00d20bb9f5cdc2da52661de13a236
Ander Coira <coirander@gmail.com>
2020-11-30 00:44:40 +0100

    Generado el comando de ayuda

--- Funcionalidad/ayuda.js
+++ Funcionalidad/ayuda.js
@@ -1,6 +1,19 @@
 const { MessageEmbed } = require("discord.js");
 const { MessageAttachment } = require('discord.js');
 
+//Comando de llamada al bot
+const botCall = '-';
+
 module.exports = ayuda => {
-    
+    ayuda.on('message', message => {
+        if (!message.content.startsWith(botCall) || message.author.bot) return;
+
+        const args = message.content.slice(botCall.length).trim().split(/ +/);
+        const command = args.shift().toLowerCase();
+
+        if(command === 'help'){
+            message.channel.send('Te helpeo');
+        }
+
+    });
 }
--- main.js
+++ main.js
@@ -3,14 +3,10 @@ const bot = new Discord.Client();
 const welcome = require('./Funcionalidad/welcome.js');
 const ayuda = require('./Funcionalidad/ayuda.js');
 
-//Comando de llamada al bot
-const botCall = 'puto ';
-
-
 bot.once('ready', () => {
     console.log('Estoy listo -- COMPILAME OTRA VEZ CUANDO HAGAS CAMBIOS PEDAZO MONGUER');
     welcome(bot);
     ayuda(bot);
 });
 
-bot.login('');
+bot.login('NzgxMjQ1NDMxNzc4MjQ2NzE2.X761gQ.1eCKFRouGsQgvm_jAt7keUcoONk');
