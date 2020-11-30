db880ac0cb1935ff90833297fb3434b2636fe45d
Ander Coira <coirander@gmail.com>
2020-11-30 20:39:48 +0100

    Cambio de estructura

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const welcome = require('./Funcionalidad/welcome.js');
-const ayuda = require('./Funcionalidad/ayuda.js');
+const ayuda = require('./Funcionalidad/help.js');
 
 bot.once('ready', () => {
     console.log('Estoy listo -- COMPILAME OTRA VEZ CUANDO HAGAS CAMBIOS PEDAZO MONGUER');
